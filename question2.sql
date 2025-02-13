513aeab6de1724b854f1fedfa334c3df9ff187da
Sahil-Renwa <sahil.renwa@gmail.com>
2025-02-13 17:53:02 +0530

    question2 file

---
+++ question2.sql
@@ -0,0 +1,13 @@
+-- ## Question 2 (INNER JOIN)
+-- List all warehouses that store electronics products, along with the total quantity and total value of electronics inventory (quantity * base_price). Only include warehouses with electronics inventory worth over $20,000. Order by total value descending.
+
+-- Expected Output:
+-- ```
+-- location    | total_quantity | total_value
+-- ------------|---------------|-------------
+-- Seattle, WA | 350           | 31497.50
+-- Boston, MA  | 365           | 25696.35
+-- Chicago, IL | 275           | 21997.25
+-- ```
+
+
