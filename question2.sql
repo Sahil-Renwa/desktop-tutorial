aa4397f4b1c82082d257626bacc862779c4e3a58
Sahil-Renwa <sahil.renwa@gmail.com>
2025-02-13 17:59:04 +0530

    Update question2.sql

--- question2.sql
+++ question2.sql
@@ -10,4 +10,11 @@
 -- Chicago, IL | 275           | 21997.25
 -- ```
 
-
+select w.location, sum(i.quantity) as total_quantity, sum(i.quantity * p.base_price) as total_value
+from warehouses w 
+inner join inventory i on w.warehouse_id = i.warehouse_id
+inner join products p on i.product_id = p.product_id
+where p.category = 'Electronics'
+group by w.location
+having sum(i.quantity * p.base_price) > 20000
+order by total_value desc;
